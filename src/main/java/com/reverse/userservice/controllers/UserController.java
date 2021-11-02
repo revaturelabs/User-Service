215aba0e8c863a16dccb71d0f5f4d3ed424fe012
Joe_Manbodh <manbodhclaude@yahoo.com>
2021-11-02 15:10:24 -0400

    update UserService Interface

--- src/main/java/com/reverse/userservice/controllers/UserController.java
+++ src/main/java/com/reverse/userservice/controllers/UserController.java
@@ -1,4 +1,21 @@
 package com.reverse.userservice.controllers;
 
+import com.reverse.userservice.services.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "user")
+@Component("UserController")
 public class UserController {
+
+    @Autowired
+    private ValidationService validationService;
+
+    @Autowired
+    public UserController(ValidationService valService) {
+        this.validationService = valService;
+    }
 }
--- src/main/java/com/reverse/userservice/services/UserService.java
+++ src/main/java/com/reverse/userservice/services/UserService.java
@@ -5,9 +5,15 @@ import com.reverse.userservice.models.User;
 import java.util.List;
 
 public interface UserService {
+
     List<User> getAllUsers();
 
     User getUserByID(Integer id);
 
+    User getUserByUsername(String username);
+
+    void registerNewUser(User user);
+
+    void updateUser(User user);
 
 }
