b7e279530bf13b00ce3b65b482e9c246f2ca56b5
Jacob J <jacobjpgit@yahoo.com>
2021-11-04 14:46:58 -0400

    Adds javadocs to Services

--- src/main/java/com/reverse/userservice/services/UserService.java
+++ src/main/java/com/reverse/userservice/services/UserService.java
@@ -9,17 +9,48 @@ import java.util.List;
 @Component("UserService")
 public interface UserService {
 
+    /**
+     * Gets all Users
+     * @return A list of all users accessable by the UserService
+     */
     List<User> getAllUsers();
 
+    /**
+     * Gets the user with the input username.
+     * @param username The username to be found.
+     * @return The user with the correct username, null if does not exist.
+     */
     User getUserByUsername(String username);
 
+    /**
+     * Registers a new User.
+     * @param user The new User to be registered.
+     */
     void registerNewUser(User user);
 
+    /**
+     * Updates an existing user.
+     * @param user The new User object after updating.
+     */
     void updateUser(User user);
 
+    /**
+     * Gets the User with the input id.
+     * @param id The id of the correct User.
+     * @return The User with the correct id, null if not found.
+     */
     public User getUserByID(Long id);
 
+    /**     TODO - Either this or registerNewUser needs to be deleted.
+     * Creates a new User.
+     * @param newUser The new user to be created.
+     */
     public void createNewUser(User newUser);
 
+    /**
+     * Finds the User referenced in the credentials and returns their correct credentials.
+     * @param credentials The input credentials.
+     * @return The accurate credentials.
+     */
     public Credentials getUserCredentials(Credentials credentials);
 }
--- src/main/java/com/reverse/userservice/services/ValidationService.java
+++ src/main/java/com/reverse/userservice/services/ValidationService.java
@@ -6,11 +6,32 @@ import com.reverse.userservice.models.ReverseJWT;
 
 public interface ValidationService {
 
+    /**
+     * Sets the secret key used for JWTs
+     * @param secret The secret key
+     */
     void setSecret(String secret);
 
+    /**
+     * Validates submitted Credentials and returns a JWT if correct.
+     * @param loginRequest The username and password stored in a credentials object.
+     * @return JWT implementation.
+     * @throws Exception Throws Exception if credentials invalid.
+     */
     ReverseJWT validateCredentials(Credentials loginRequest) throws Exception;
 
+    /**
+     * Checks that a jwt is valid.
+     * @param jwt The jwt to be validated.
+     * @return true if valid, false if invalid.
+     */
     boolean validateJwt(ReverseJWT jwt);
 
+    /**
+     * Checks if a jwt is valid, and that the value matches a userID.
+     * @param jwt The jwt to be validated.
+     * @param userID The long to be checked against the jwt body.
+     * @return true if valid, false if invalid.
+     */
     boolean validateJwt(ReverseJWT jwt, long userID);
 }
