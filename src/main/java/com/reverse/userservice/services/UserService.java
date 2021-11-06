fa7a9930fc6efbeb4c30bc766cc5a2864ece4c07
Asher Rosenbaum <90282324+ARosenbaum98@users.noreply.github.com>
2021-11-05 20:49:50 -0500

    Update UserService.java
    fix an error I made while fixing merge conflicts

--- src/main/java/com/reverse/userservice/services/UserService.java
+++ src/main/java/com/reverse/userservice/services/UserService.java
@@ -12,45 +12,45 @@ public interface UserService {
      * Gets all Users
      * @return A list of all users accessable by the UserService
      */
-    public List<User> getAllUsers();
+    List<User> getAllUsers();
 
     /**
      * Gets the user with the input username.
      * @param username The username to be found.
      * @return The user with the correct username, null if does not exist.
      */
-    public User getUserByUsername(String username);
+    User getUserByUsername(String username);
 
 
     /**
      * Registers a new User.
      * @param user The new User to be registered.
      */
-    public void registerNewUser(User user);
+    void registerNewUser(User user);
   
     /**
      * Updates an existing user.
      * @param user The new User object after updating.
      */
-    public void updateUser(User user);
+    void updateUser(User user);
 
     /**
      * Gets the User with the input id.
      * @param id The id of the correct User.
      * @return The User with the correct id, null if not found.
      */
-    public User getUserByID(Long id);
+    User getUserByID(Long id);
 
     /**     TODO - Either this or registerNewUser needs to be deleted.
      * Creates a new User.
      * @param newUser The new user to be created.
      */
-    public void createNewUser(User newUser);
+    void createNewUser(User newUser);
   
     /**
      * Finds the User referenced in the credentials and returns their correct credentials.
      * @param credentials The input credentials.
      * @return The accurate credentials.
      */
-    public Credentials getUserCredentials(Credentials credentials);
+    Credentials getUserCredentials(Credentials credentials);
 }
