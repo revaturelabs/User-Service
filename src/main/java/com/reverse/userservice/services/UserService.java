2d2cd7b7b16cb4346cbd45d3ed7a939a74f2605e
Jacob J <jacobjpgit@yahoo.com>
2021-11-11 15:32:39 -0500

    Adds comments to UserService

--- src/main/java/com/reverse/userservice/services/UserService.java
+++ src/main/java/com/reverse/userservice/services/UserService.java
@@ -7,14 +7,37 @@ import java.util.List;
 
 public interface UserService {
 
+    /**
+     * Returns a list of all the usesrs.
+     * @return List of all current users.
+     */
     List<User> getAllUsers();
 
+    /**
+     * Returns the user with this username, null if no user exists.
+     * @param username The username to search for.
+     * @return The User with the input username, null if none.
+     */
     User getUserByUsername(String username);
 
+    /**
+     * Updates the User
+     * @param user The updated User
+     */
     void updateUser(User user);
 
+    /**
+     * Returns the user with this id, null if no user exists.
+     * @param id The id to search for.
+     * @return The User with the input id, null if none.
+     */
     public User getUserByID(Long id);
 
+    /**
+     * Creates a new User and returns the id.
+     * @param newUser The new User.
+     * @return The id of the new user.
+     */
     Long createNewUser(User newUser);
 
 }
